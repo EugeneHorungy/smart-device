51582adbef295a666e6c49718dc93d4a955133a2
EugeneHorungy <77688070+EugeneHorungy@users.noreply.github.com>
2021-11-09 07:44:15 +0300

    adds main content

--- build/js/script.js
+++ build/js/script.js
@@ -3,34 +3,12 @@
 const promoInfo = document.querySelector('.promo__info');
 const getConsultationButton = promoInfo.querySelector('a');
 const maxWidthMobile = window.matchMedia('(max-width: 767px)');
-// const accordeonSwitchers = document.querySelectorAll('.accordeon-switch');
-// const accordeons = document.querySelectorAll('.accordeon');
+const accordeonSwitchers = document.querySelectorAll('.accordeon-switcher');
+const accordeons = document.querySelectorAll('.accordeon');
 
 if (maxWidthMobile.matches) {
   getConsultationButton.textContent = 'Бесплатная консультация';
 }
-
-// $('.questions__item-title').on('click', function() {
-//   if ($(this).hasClass("questions__item-title--active")) {
-//     $(this).removeClass('questions__item-title--active');
-//   } else {
-//     $('.questions__item-title').removeClass('questions__item-title--active');
-//     $(this).addClass('questions__item-title--active');
-//   }
-// });
-
-// accordeonSwitchers.addEventListener('click', function (item) {
-//   if (item.classList.contains('accordeon-switcher--active')) {
-//     item.classList.remove('accordeon-switcher--active');
-//   } else {
-//     item.classList.add('accordeon-switcher--active');
-//   }
-// });
-
-// Slider services
-
-const accordeonSwitchers = document.querySelectorAll('.accordeon-switcher');
-const accordeons = document.querySelectorAll('.accordeon');
 
 const removeSwitcherActiveClass = () => {
   Array.from(accordeonSwitchers).forEach((item) => {
@@ -44,19 +22,8 @@ const removeAccordeonActiveClass = () => {
   });
 };
 
-// Array.from(accordeonSwitchers).forEach((item, i) => {
-//   item.addEventListener('click', () => {
-//     removeSwitcherActiveClass();
-//     removeAccordeonActiveClass();
-//     accordeonSwitchers[i].classList.add('accordeon-switcher--active');
-//     accordeons[i].classList.add('accordeon--active');
-//   });
-// });
-
 Array.from(accordeonSwitchers).forEach((item, i) => {
   item.addEventListener('click', () => {
-    // removeSwitcherActiveClass();
-    // removeAccordeonActiveClass();
     if (accordeonSwitchers[i].classList.contains('accordeon-switcher--active')) {
       accordeonSwitchers[i].classList.remove('accordeon-switcher--active');
       accordeons[i].classList.remove('accordeon--active');
